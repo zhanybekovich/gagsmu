e00229ea060217abe722d3931a1bdfec0621fbb1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-04-05 14:45:35 +0600

    Add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { useState } from "react";
+import { Link } from "gatsby";
+
+import "./navbar.scss";
+
+const links = ["Буйруктар", "Усулдук көрсөтмөлөр", "Типтүү устав", "Этаптар"];
+
+const Navbar = () => {
+  const [stickyNav, setStickyNav] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 100) {
+      setStickyNav(true);
+    } else {
+      setStickyNav(false);
+    }
+  };
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+  return (
+    <nav className={stickyNav ? "navbar--active" : "navbar"}>
+      <div className="container">
+        <div className="navbar-inner">
+          <Link className="navbar-link" to="/">
+            Башкы бет
+          </Link>
+          <button className="navbar-toggle" onClick={handleMenuClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+            </svg>
+          </button>
+          <ul className={menuOpen ? "navbar-menu" : "navbar-menu--active"}>
+            {links.map((link, index) => (
+              <li key={index + link}>
+                <Link className="navbar-link" to={link}>
+                  {link}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 import * as React from "react";
+import "../styles/styles.scss";
+import Navbar from "../components/navbar/Navbar";
 
 const IndexPage = () => {
   return (
     <main>
-      <h1>Hello</h1>
+      <header className="header">
+        <Navbar />
+      </header>
     </main>
   );
 };
 
 export default IndexPage;
 
-export const Head = () => <title>Home Page</title>;
+export const Head = () => (
+  <>
+    <title>Жергиликтуу уставдарды кабыл алуу жол картасы</title>
+    <meta
+      name="description"
+      content="Жергиликтуу уставдарды кабыл алуу жол картасы"
+    />
+  </>
+);
