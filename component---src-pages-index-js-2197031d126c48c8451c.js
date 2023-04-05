0e1aa0392b31413b35ad43f57d24587b498d20cb
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-04-05 13:06:57 +0600

    Updates

---
+++ component---src-pages-index-js-2197031d126c48c8451c.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkroadmap=self.webpackChunkroadmap||[]).push([[678],{6558:function(e,l,n){n.r(l),n.d(l,{Head:function(){return a}});var t=n(7294);l.default=()=>t.createElement("main",null,t.createElement("h1",null,"Hello"));const a=()=>t.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-2197031d126c48c8451c.js.map
