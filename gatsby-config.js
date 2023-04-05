d831008092dfee285c6003199f9ec726a3ed581b
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-04-05 12:48:10 +0600

    Project init

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `roadmap`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: ["gatsby-plugin-sass"],
+  pathPrefix: "/reponame",
+};
